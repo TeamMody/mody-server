7b3f5b66b7b5baae42cd023b18f1ccf0597391aa
shimfff <shimseawon1510@gmail.com>
2025-01-08 14:19:54 +0900

    :wrench: chore : stop.sh 수정

--- script/stop.sh
+++ script/stop.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker-compose -f /home/ec2-user/app/docker-compose.yml down || true
+docker-compose -f /home/ec2-user/app/docker-compose-dev.yml down || true
+
+# 안 사용하는 이미지 정리
+docker image prune -f
