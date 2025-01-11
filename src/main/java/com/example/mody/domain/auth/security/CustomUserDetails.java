b2eb4bb9d3b9f1651dbc57b8d4ae06880a1125c5
jher235 <tim668666@gmail.com>
2025-01-11 18:58:05 +0900

    :sparkles: [#13] feature(auth): getMember 메서드 추가

--- src/main/java/com/example/mody/domain/auth/security/CustomUserDetails.java
+++ src/main/java/com/example/mody/domain/auth/security/CustomUserDetails.java
@@ -60,4 +60,8 @@ public class CustomUserDetails implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+    public Member getMember(){
+        return this.member;
+    }
 }
