7dd9850fa3e06c897772edaa1a818035f6b32ee5
jher235 <tim668666@gmail.com>
2025-01-16 00:21:05 +0900

    :sparkles: [#26] feature: 좋아요 누른 게시글 목록 조회의 비즈니스 로직 수정 - 해당 기능의 커서를 좋아요 ID로 수정하며 비즈니스 로직을 수정

--- src/main/java/com/example/mody/domain/post/service/PostQueryServiceImpl.java
+++ src/main/java/com/example/mody/domain/post/service/PostQueryServiceImpl.java
@@ -2,11 +2,19 @@ package com.example.mody.domain.post.service;
 
 import com.example.mody.domain.bodytype.entity.BodyType;
 import com.example.mody.domain.bodytype.service.BodyTypeService;
+import com.example.mody.domain.exception.PostException;
 import com.example.mody.domain.member.entity.Member;
 import com.example.mody.domain.post.dto.response.PostListResponse;
+import com.example.mody.domain.post.dto.response.PostResponse;
+import com.example.mody.domain.post.dto.response.recode.LikedPostsResponse;
+import com.example.mody.domain.post.entity.Post;
+import com.example.mody.domain.post.entity.mapping.MemberPostLike;
+import com.example.mody.domain.post.repository.MemberPostLikeRepository;
 import com.example.mody.domain.post.repository.PostRepository;
 import com.example.mody.global.common.exception.RestApiException;
 import com.example.mody.global.common.exception.code.status.GlobalErrorStatus;
+import com.example.mody.global.common.exception.code.status.MemberPostLikeErrorStatus;
+import com.example.mody.global.common.exception.code.status.PostErrorStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,10 +23,9 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class PostQueryServiceImpl implements PostQueryService {
-    private final PostRepository postRepository;
-
     private final BodyTypeService bodyTypeService;
-
+    private final PostRepository postRepository;
+    private final MemberPostLikeRepository memberPostLikeRepository;
 
     @Override
     public PostListResponse getPosts(Member member, Integer size, Long cursor) {
@@ -27,12 +34,14 @@ public class PostQueryServiceImpl implements PostQueryService {
             throw new RestApiException(GlobalErrorStatus.NEGATIVE_PAGE_SIZE_REQUEST);
         }
 
+        Optional<Post> cursorPost = getCursorPost(cursor);
+
         if(member != null){
             Optional<BodyType> bodyTypeOptional = bodyTypeService.findLastBodyType(member);
-            return postRepository.getPostList(cursor, size, member, bodyTypeOptional);
+            return postRepository.getPostList(cursorPost, size, member, bodyTypeOptional);
         }
 
-        return postRepository.getPostList(cursor, size, member, Optional.empty());
+        return postRepository.getPostList(cursorPost, size, member, Optional.empty());
     }
 
     @Override
@@ -40,7 +49,12 @@ public class PostQueryServiceImpl implements PostQueryService {
         if(size<=0){
             throw new RestApiException(GlobalErrorStatus.NEGATIVE_PAGE_SIZE_REQUEST);
         }
-        return postRepository.getLikedPosts(cursor, size, member);
+        LikedPostsResponse likedPostsResponse = postRepository.getLikedPosts(cursor, size, member);
+
+        PostResponse postResponse = likedPostsResponse.postResponses().getLast();
+        MemberPostLike memberPostLike = findByMemberAndPostId(member, postResponse.getPostId());
+
+        return PostListResponse.from(likedPostsResponse.hasNext(), likedPostsResponse.postResponses(), memberPostLike.getId());
     }
 
     @Override
@@ -50,4 +64,19 @@ public class PostQueryServiceImpl implements PostQueryService {
         }
         return postRepository.getMyPosts(cursor, size, member);
     }
+
+    public Optional getCursorPost(Long cursor){
+        return cursor != null ? Optional.ofNullable(findById(cursor)) : Optional.empty();
+    }
+
+    public Post findById(Long id){
+        return postRepository.findById(id).orElseThrow(()->
+                new PostException(PostErrorStatus.MEMBER_NOT_FOUND));
+    }
+
+    public MemberPostLike findByMemberAndPostId(Member member, Long postId){
+        return memberPostLikeRepository.findByMemberAndPostId(member, postId).orElseThrow(()
+                -> new PostException(MemberPostLikeErrorStatus.LIKE_NOT_FOUND));
+    }
+
 }
