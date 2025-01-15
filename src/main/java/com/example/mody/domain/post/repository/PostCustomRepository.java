88cf9b5107bbcc361b854dda6b07dc669bc887ed
jher235 <tim668666@gmail.com>
2025-01-15 17:50:58 +0900

    :recycle: [#26] refactor: 전체 조회 시 custom cursor를 사용하여 정렬 기준을 정하도록 수정

--- src/main/java/com/example/mody/domain/post/repository/PostCustomRepository.java
+++ src/main/java/com/example/mody/domain/post/repository/PostCustomRepository.java
@@ -3,13 +3,15 @@ package com.example.mody.domain.post.repository;
 import com.example.mody.domain.bodytype.entity.BodyType;
 import com.example.mody.domain.member.entity.Member;
 import com.example.mody.domain.post.dto.response.PostListResponse;
+import com.example.mody.domain.post.dto.response.recode.LikedPostsResponse;
+import com.example.mody.domain.post.entity.Post;
 import jakarta.persistence.EntityManager;
 
 import java.util.Optional;
 
 public interface PostCustomRepository {
 
-    public PostListResponse getPostList(Long cursor, Integer size, Member member, Optional<BodyType> bodyType);
-    public PostListResponse getLikedPosts(Long cursor, Integer size, Member member);
+    public PostListResponse getPostList(Optional<Post> cursorPost, Integer size, Member member, Optional<BodyType> bodyType);
+    public LikedPostsResponse getLikedPosts(Long cursor, Integer size, Member member);
     public PostListResponse getMyPosts(Long cursor, Integer size, Member member);
 }
