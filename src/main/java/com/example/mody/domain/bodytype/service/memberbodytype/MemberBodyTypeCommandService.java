827034e2666b42ca94c0dab809e493ac02405294
sshnote <springssh0504@naver.com>
2025-01-24 21:06:31 +0900

    ♻️ [#59] refactor: 체형 분석 API 요청 수정"
    체형 분석 API에서 요청 받는 DTO를 수정하였습니다.

--- src/main/java/com/example/mody/domain/bodytype/service/memberbodytype/MemberBodyTypeCommandService.java
+++ src/main/java/com/example/mody/domain/bodytype/service/memberbodytype/MemberBodyTypeCommandService.java
@@ -1,6 +1,6 @@
 package com.example.mody.domain.bodytype.service.memberbodytype;
 
-import com.example.mody.domain.bodytype.dto.BodyTypeAnalysisResponse;
+import com.example.mody.domain.bodytype.dto.response.BodyTypeAnalysisResponse;
 import com.example.mody.domain.member.entity.Member;
 
 public interface MemberBodyTypeCommandService {
