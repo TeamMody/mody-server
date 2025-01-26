c7d933368bb950b8b259314400a07870b880bc5e
jher235 <tim668666@gmail.com>
2025-01-27 02:07:57 +0900

    :sparkles: [#75] feature: member프로필 조회 비즈니스 로직 작성

--- src/main/java/com/example/mody/domain/member/service/MemberQueryService.java
+++ src/main/java/com/example/mody/domain/member/service/MemberQueryService.java
@@ -1,7 +1,9 @@
 package com.example.mody.domain.member.service;
 
+import com.example.mody.domain.member.dto.response.MemberProfileResponse;
 import com.example.mody.domain.member.entity.Member;
 
 public interface MemberQueryService {
     Member findMemberById(Long memberId);
+    MemberProfileResponse getMyProfile(Member member);
 }
