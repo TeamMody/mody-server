3e7af5fccd47f4d94e994134764bdf8f28275911
shimfff <shimseawon1510@gmail.com>
2025-02-04 22:12:10 +0900

    :recycle: refactor : 스웨거 명세 인터페이스로 분리

--- src/main/java/com/example/mody/domain/recommendation/controller/RecommendationController.java
+++ src/main/java/com/example/mody/domain/recommendation/controller/RecommendationController.java
@@ -25,33 +25,23 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/recommendations")
-public class RecommendationController {
+public class RecommendationController implements RecommendationControllerInterface {
 
     private final RecommendationQueryService recommendationQueryService;
     private final RecommendationCommendService recommendationCommendService;
 
-    @Operation(summary = "추천 카테고리 조회 API", description = "추천을 위한 스타일, 어필 카테고리를 반환하는 API")
+
+    // todo : 피그마에 있는 카테고리 순서대로 주기
+    // 추천 카테고리 조회
     @GetMapping("/categories")
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "스타일 카테고리 조회 성공",
-                    content = @Content(schema = @Schema(implementation = CategoryResponse.class))
-            )
-    })
     public BaseResponse<CategoryResponse> getStyleCategories() {
         //서비스에서 데이터를 조회하여 반환
         CategoryResponse categoryResponse = recommendationQueryService.getCategories();
         return BaseResponse.onSuccess(categoryResponse);
     }
 
-
-    @Operation(summary = "스타일 추천 좋아요 API", description = "스타일 추천에 대한 좋아요 기능")
+    // 스타일 추천 좋아요
     @PostMapping("/{recommendationId}/like")
-    @ApiResponses({
-            @ApiResponse(responseCode = "COMMON200", description = "스타일 추천에 좋아요 성공"),
-            @ApiResponse(responseCode = "RECOMMENDATION404", description = "요청한 스타일 추천 결과물이 존재하지 않는 경우")
-    })
     public BaseResponse<RecommendLikeResponse> toggleStyleLike(
             @PathVariable(name = "recommendationId") Long recommendationId,
             @AuthenticationPrincipal CustomUserDetails customUserDetails) {
@@ -59,32 +49,8 @@ public class RecommendationController {
         return BaseResponse.onSuccess(response);
     }
 
-
-    @Operation(summary = "추천 결과 조회 API", description = "사용자가 추천받은 결과를 마이페이지에서 조회하는 api입니다.")
+    // 추천 결과 조회
     @GetMapping
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "스타일 추천 성공",
-                    content = @Content(schema = @Schema(implementation = RecommendResponses.class))
-            ),
-            @ApiResponse(
-                    responseCode = "RECOMMENDATION404",
-                    description = "사용자의 체형 정보를 찾을 수 없음",
-                    content = @Content(
-                            mediaType = "application/json",
-                            examples = @ExampleObject(
-                                    value = """
-                                            {
-                                              "timestamp": "2025-01-17T00:48:53.9237864",
-                                              "code": "MEMBER_BODY_TYPE404",
-                                              "message": "체형 분석 결과를 찾을 수 없습니다."
-                                            }
-                                            """
-                            )
-                    )
-            )
-    })
     public BaseResponse<RecommendResponses> getRecommendations(
             @AuthenticationPrincipal CustomUserDetails customUserDetails,
             @RequestParam(name = "cursor", required = false) Long cursor,
@@ -94,67 +60,8 @@ public class RecommendationController {
         return BaseResponse.onSuccess(responses);
     }
 
-    @Operation(summary = "스타일 추천 API", description = "스타일 분석 후 그 결과를 반환합니다.")
     @PostMapping("/style-analysis")
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "스타일 추천 성공",
-                    content = @Content(schema = @Schema(implementation = RecommendResponse.class))
-            ),
-            @ApiResponse(
-                    responseCode = "MEMBER_BODY_TYPE404",
-                    description = "사용자의 체형 정보를 찾을 수 없음",
-                    content = @Content(
-                            mediaType = "application/json",
-                            examples = @ExampleObject(
-                                    value = """
-						{
-						  "timestamp": "2025-01-17T00:48:53.9237864",
-						  "code": "MEMBER_BODY_TYPE404",
-						  "message": "체형 분석 결과를 찾을 수 없습니다."
-						}
-						"""
-                            )
-                    )
-            ),
-            @ApiResponse(
-                    responseCode = "COMMON402",
-                    description = "카테고리 리스트가 비어있을 때 발생합니다. " +
-                            "선호하는 스타일/ 선호하지 않는 스타일/ 보여주고 싶은 이미지 목록으로 표시됩니다.",
-                    content = @Content(
-                            mediaType = "application/json",
-                            examples = @ExampleObject(
-                                    value = """
-							{
-								"timestamp": "2025-01-25T15:57:08.7901651",
-								"code": "COMMON402",
-								"message": "Validation Error입니다.",
-								"result": {
-									"preferredStyles": "선호하는 스타일 목록은 비어 있을 수 없습니다."
-								}
-							}
-							"""
-                            )
-                    )
-            ),
-            @ApiResponse(
-                    responseCode = "ANALYSIS108",
-                    description = "GPT 응답 형식이 적절하지 않을 때 발생합니다.",
-                    content = @Content(
-                            mediaType = "application/json",
-                            examples = @ExampleObject(
-                                    value = """
-						{
-							"timestamp": "2025-01-25T16:02:42.4014717",
-							"code": "ANALYSIS108",
-							"message": "GPT가 올바르지 않은 답변을 했습니다. 관리자에게 문의하세요."
-						}
-						"""
-                            )
-                    )
-            )
-    })
+    // 스타일 추천 API
     public BaseResponse<RecommendResponse> recommendStyle(
             @Valid @RequestBody RecommendRequest request,
             @AuthenticationPrincipal CustomUserDetails customUserDetails) {
@@ -164,67 +71,8 @@ public class RecommendationController {
         return BaseResponse.onSuccess(response);
     }
 
-    @Operation(summary = "패션 아이템 추천 API", description = "패션아이템 분석 후 그 결과를 반환합니다.")
+    // 스타일 아이템 추천 API
     @PostMapping("/fashion-item-analysis")
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "패션 아이템 추천 성공",
-                    content = @Content(schema = @Schema(implementation = RecommendResponse.class))
-            ),
-            @ApiResponse(
-                    responseCode = "MEMBER_BODY_TYPE404",
-                    description = "사용자의 체형 정보를 찾을 수 없음",
-                    content = @Content(
-                            mediaType = "application/json",
-                            examples = @ExampleObject(
-                                    value = """
-						{
-						  "timestamp": "2025-01-17T00:48:53.9237864",
-						  "code": "MEMBER_BODY_TYPE404",
-						  "message": "체형 분석 결과를 찾을 수 없습니다."
-						}
-						"""
-                            )
-                    )
-            ),
-            @ApiResponse(
-                    responseCode = "COMMON402",
-                    description = "카테고리 리스트가 비어있을 때 발생합니다. " +
-                            "선호하는 스타일/ 선호하지 않는 스타일/ 보여주고 싶은 이미지 목록으로 표시됩니다.",
-                    content = @Content(
-                            mediaType = "application/json",
-                            examples = @ExampleObject(
-                                    value = """
-							{
-								"timestamp": "2025-01-25T15:57:08.7901651",
-								"code": "COMMON402",
-								"message": "Validation Error입니다.",
-								"result": {
-									"preferredStyles": "선호하는 스타일 목록은 비어 있을 수 없습니다."
-								}
-							}
-							"""
-                            )
-                    )
-            ),
-            @ApiResponse(
-                    responseCode = "ANALYSIS108",
-                    description = "GPT 응답 형식이 적절하지 않을 때 발생합니다.",
-                    content = @Content(
-                            mediaType = "application/json",
-                            examples = @ExampleObject(
-                                    value = """
-						{
-							"timestamp": "2025-01-25T16:02:42.4014717",
-							"code": "ANALYSIS108",
-							"message": "GPT가 올바르지 않은 답변을 했습니다. 관리자에게 문의하세요."
-						}
-						"""
-                            )
-                    )
-            )
-    })
     public BaseResponse<RecommendResponse> recommendFashionItem(
             @Valid @RequestBody RecommendRequest request,
             @AuthenticationPrincipal CustomUserDetails customUserDetails) {
