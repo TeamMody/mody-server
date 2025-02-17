d9d992dd63249f2861c20e9c408624435af5f9ce
jher235 <tim668666@gmail.com>
2025-02-18 01:25:20 +0900

    :recycle: [#131] refactor: 쿼리 최적화 및 코드 분리 - 기존 인덱스를 타지 못하던 정렬 기준 수정 - 인덱스 사용이 불가했던 커스텀 커서를 지우고 비즈니스 로직으로 상황별 최적화 - 중복되는 jpaQueryFactory 분리

--- src/main/java/com/example/mody/domain/post/repository/PostCustomRepository.java
+++ src/main/java/com/example/mody/domain/post/repository/PostCustomRepository.java
@@ -5,13 +5,15 @@ import com.example.mody.domain.member.entity.Member;
 import com.example.mody.domain.post.dto.response.PostListResponse;
 import com.example.mody.domain.post.dto.response.recode.LikedPostsResponse;
 import com.example.mody.domain.post.entity.Post;
-import jakarta.persistence.EntityManager;
 
 import java.util.Optional;
 
 public interface PostCustomRepository {
 
-    public PostListResponse getPostList(Optional<Post> cursorPost, Integer size, Member member, Optional<BodyType> bodyType);
+    public PostListResponse getBodyTypePosts(Optional<Post> cursorPost, Integer size, Member member, BodyType bodyType);
+    public PostListResponse getOtherBodyTypePosts(Optional<Post> cursorPost, Integer size, Member member, BodyType bodyType);
     public LikedPostsResponse getLikedPosts(Long cursor, Integer size, Member member);
     public PostListResponse getMyPosts(Long cursor, Integer size, Member member);
+    public PostListResponse getRecentPosts(Long cursor, Integer size, Member member);
+
 }
