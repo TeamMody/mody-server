6daec9b8990271f815a2d7202f1da11bdff4a1e1
sshnote <springssh0504@naver.com>
2025-02-18 16:37:31 +0900

    ♻️ [#135] refactor: 컨트롤러 책임 분리
    스웨거 명세와 실제 로직을 분리하기 위해 인터페이스를 도입하였습니다.

--- src/main/java/com/example/mody/domain/bodytype/controller/BodyTypeController.java
+++ src/main/java/com/example/mody/domain/bodytype/controller/BodyTypeController.java
@@ -7,78 +7,23 @@ import com.example.mody.domain.bodytype.service.memberbodytype.MemberBodyTypeCom
 import com.example.mody.domain.bodytype.service.memberbodytype.MemberBodyTypeQueryService;
 import com.example.mody.global.common.base.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "체형 분석", description = "체형 분석 API")
+@Tag(name = "체형 분석", description = "체형 분석 관련 API")
 @RestController
 @RequestMapping("/body-analysis")
 @RequiredArgsConstructor
-public class BodyTypeController {
+public class BodyTypeController implements BodyTypeControllerInterface {
 
     private final MemberBodyTypeCommandService memberBodyTypeCommandService;
     private final MemberBodyTypeQueryService memberBodyTypeQueryService;
 
     @PostMapping("/result")
-    @Operation(summary = "체형 분석 API", description = "OpenAi를 사용해서 사용자의 체형을 분석하는 API입니다. Request Body에는 질문에 맞는 답변 목록을 보내주세요.")
-    @ApiResponses({
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200",description = "체형 분석 성공"),
-            @ApiResponse(
-                    responseCode = "401",
-                    description = "Access Token이 필요합니다.",
-                    content = @Content(
-                            mediaType = "application/json",
-                            examples = @ExampleObject(
-                                    value = """
-						{
-							"timestamp": "2025-01-26T15:15:54.334Z",
-							"code": "COMMON401",
-							"message": "인증이 필요합니다."
-						}
-						"""
-                            )
-                    )
-            ),
-			@ApiResponse(
-					responseCode = "404",
-					description = "체형을 찾을 수 없습니다.",
-					content = @Content(
-							mediaType = "application/json",
-							examples = @ExampleObject(
-									value = """
-						{
-							"timestamp": "2025-01-26T15:15:54.334Z",
-							"code": "BODY_TYPE404",
-							"message": "체형을 찾을 수 없습니다."
-						}
-						"""
-							)
-					)
-			),
-            @ApiResponse(
-                    responseCode = "500",
-                    description = "GPT가 적절한 응답을 하지 못 했습니다.",
-                    content = @Content(
-                            mediaType = "application/json",
-                            examples = @ExampleObject(
-                                    value = """
-						{
-							"timestamp": "2025-01-26T15:15:54.334Z",
-							"code": "ANALYSIS108",
-							"message": "GPT가 올바르지 않은 답변을 했습니다. 관리자에게 문의하세요."
-						}
-						"""
-                            )
-                    )
-            ),
-    })
+	@Operation(summary = "체형 분석 API", description = "OpenAI를 사용해서 사용자의 체형을 분석하는 API입니다. Request Body에는 질문에 맞는 답변 목록을 보내주세요.")
     public BaseResponse<BodyTypeAnalysisResponse> analyzeBodyType(
             @AuthenticationPrincipal CustomUserDetails customUserDetails,
             @Valid @RequestBody BodyTypeAnalysisRequest request
@@ -88,57 +33,6 @@ public class BodyTypeController {
 
     @GetMapping("/result")
     @Operation(summary = "체형 분석 결과 조회 API", description = "사용자의 체형 분석 결과를 받아 오는 API입니다.")
-    @ApiResponses({
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200",description = "체형 분석 결과 조회 성공"),
-            @ApiResponse(
-                    responseCode = "400",
-                    description = "체형 분석 결과를 처리하는 중 JSON 파싱에 실패했습니다.",
-                    content = @Content(
-                            mediaType = "application/json",
-                            examples = @ExampleObject(
-                                    value = """
-						{
-							"timestamp": "2025-01-26T15:15:54.334Z",
-							"code": "JSON_PARSING400",
-							"message": "체형 분석 결과를 처리하는 중 JSON 파싱에 실패했습니다."
-						}
-						"""
-                            )
-                    )
-            ),
-            @ApiResponse(
-                    responseCode = "401",
-                    description = "Access Token이 필요합니다.",
-                    content = @Content(
-                            mediaType = "application/json",
-                            examples = @ExampleObject(
-                                    value = """
-						{
-							"timestamp": "2025-01-26T15:15:54.334Z",
-							"code": "COMMON401",
-							"message": "인증이 필요합니다."
-						}
-						"""
-                            )
-                    )
-            ),
-            @ApiResponse(
-                    responseCode = "404",
-                    description = "체형 분석 결과를 찾을 수 없습니다.",
-                    content = @Content(
-                            mediaType = "application/json",
-                            examples = @ExampleObject(
-                                    value = """
-						{
-							"timestamp": "2025-01-26T15:15:54.334Z",
-							"code": "MEMBER_BODY_TYPE404",
-							"message": "체형 분석 결과를 찾을 수 없습니다."
-						}
-						"""
-                            )
-                    )
-            ),
-    })
     public BaseResponse<BodyTypeAnalysisResponse> getBodyType(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
         return BaseResponse.onSuccess(memberBodyTypeQueryService.getBodyTypeAnalysis(customUserDetails.getMember()));
     }
