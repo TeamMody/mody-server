5e02cb143f9fa4ee15f5b42cdf563f9bea10033f
seoshinehyo <springssh0504@naver.com>
2025-03-30 18:32:06 +0900

    ✨ [#150] feature: 오늘 날씨에 어울리는 패션 추천 API 구현

--- src/main/java/com/example/mody/domain/recommendation/service/RecommendationCommendService.java
+++ src/main/java/com/example/mody/domain/recommendation/service/RecommendationCommendService.java
@@ -2,14 +2,18 @@ package com.example.mody.domain.recommendation.service;
 
 import com.example.mody.domain.member.entity.Member;
 import com.example.mody.domain.recommendation.dto.request.RecommendRequest;
+import com.example.mody.domain.recommendation.dto.request.WeatherRecommendRequest;
 import com.example.mody.domain.recommendation.dto.response.RecommendResponse;
 import com.example.mody.domain.recommendation.dto.response.RecommendLikeResponse;
+import com.example.mody.domain.recommendation.dto.response.analysis.WeatherStyleAnalysisResponse;
 
 public interface RecommendationCommendService {
 
     RecommendResponse recommendStyle(Member member, RecommendRequest request);
 
     RecommendResponse recommendFashionItem(Member member, RecommendRequest request);
+
+    RecommendResponse recommendWeatherStyle(Member member, WeatherRecommendRequest request);
 
     RecommendLikeResponse toggleLike(Long recommendationId, Member member);
 }
